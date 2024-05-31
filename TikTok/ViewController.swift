0ce3003370f1809339aea7f199542dfd8661ac8f
Olivier Marszałkowski <marszalkowskiolivier@gmail.com>
2024-05-31 12:46:48 +0200

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Olivier Marszałkowski on 31/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
